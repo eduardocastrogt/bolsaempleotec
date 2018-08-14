05472c8c2402f176a332a990b162ff55aa666710
Oscar Eduardo Castro <oscar.castro@cemaco.com>
2018-08-14 11:52:20 -0600

    Generando user js en routes

---
+++ routes/user.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var express = require('express');
+/*Controller de user acá*/
+/*Middelwares acá*/
+
+var api = express.Router();
+
+/*Direcciones de los endpoints acá*/
+
+module.exports = api;
